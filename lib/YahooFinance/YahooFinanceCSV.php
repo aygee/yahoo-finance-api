332555eaa1f75cf28bbe47a0c74918adc4f96405
Adrian Gunawan <adrn.social@gmail.com>
2012-03-08 08:27:01 +1100

    added yahoo finance api using csv
    Getting historical prices from csv is much faster and reliable than
    using the yql on yahoo.finance.historicaldata

---
+++ lib/YahooFinance/YahooFinanceCSV.php
@@ -0,0 +1,41 @@
+<?php
+
+class YahooFinanceCSV {
+	private $url = "http://ichart.finance.yahoo.com/table.csv";
+
+	/* getCSV
+	 * @params $symbol stock symbol, eg: asx.ax, bkl.ax
+	 * @params $startdate optional start date. If not specified, it will return result from the earliest possible record
+	 * @params $enddate optional end date. If not specified, it will return result up to the latest possible record
+	 * @params $freq optional frequency. Possible values: 'd' for daily, 'm' for monthly, 'y' for yearly. Defaulted to 'd'
+	 *
+	 * @return csv
+	 */
+	public function getQuotesCSV($symbol, $startdate=NULL, $enddate=NULL, $freq='d') {
+		$url = $this->url . "?s={$symbol}";
+		
+		if (is_string($startdate) && !empty($startdate)) {
+			$startdate = new DateTime($startdate);
+			$url .= "&a=" . $startdate->format('m');  // start month
+			$url .= "&b=" . $startdate->format('d');  // start day
+			$url .= "&c=" . $startdate->format('y');  // start year
+		}
+
+		if (is_string($enddate) && !empty($enddate)) {
+			$enddate = new DateTime($enddate);
+			$url .= "&d=" . $enddate->format('m');    // end month
+			$url .= "&e=" . $enddate->format('d');    // end day
+			$url .= "&f=" . $enddate->format('y');    // end year
+		}
+
+		$url .= "&g=" . $freq;
+
+		return $this->run($url);
+	}
+
+	private function run($url){
+		$session = curl_init($url);
+		curl_setopt($session, CURLOPT_RETURNTRANSFER,true);      
+		return curl_exec($session);    		
+	}
+}
